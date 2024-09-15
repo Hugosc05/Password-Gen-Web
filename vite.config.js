b1e25cc65d9a0a647fe5f6724b26f7280c3d872f
Hugosc05 <165696114+Hugosc05@users.noreply.github.com>
2024-09-15 17:00:30 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
-export default {
   build: {
     rollupOptions: {
       external: ["@fortawesome/react-fontawesome"]
     }
   }
-}
+})
